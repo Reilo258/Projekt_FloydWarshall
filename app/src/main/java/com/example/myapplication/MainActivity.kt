7440a39cff3da26c3e806fffffd8f94d3a3639f2
Palczyk69 <lukasz.holowacz5@gmail.com>
2023-09-22 12:14:39 +0200

    Dodanie 2 przyciskow, inicjowanie zmiennych

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttons: List<ImageButton>
+    private lateinit var slider: SeekBar
+    private lateinit var wagalbl: TextView
+    private lateinit var floydWarshallbtn: Button
+    private lateinit var ustawwagebtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttons = listOf(
+            findViewById(R.id.button1),
+            findViewById(R.id.button2),
+            findViewById(R.id.button3),
+            findViewById(R.id.button4),
+            findViewById(R.id.button5)
+        )
+        slider = findViewById(R.id.slider)
+        wagalbl = findViewById(R.id.wagalbl)
+        ustawwagebtn = findViewById(R.id.wagabtn)
+        floydWarshallbtn = findViewById(R.id.algorytmbtn)
+
     }
 }
